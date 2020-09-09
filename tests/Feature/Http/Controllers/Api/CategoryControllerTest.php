94aaef49397e6e01cdd29c30eee99c477e756bbd
Dhoi Almeida <26941289+dhoialmeida@users.noreply.github.com>
2020-09-09 09:27:22 -0300

    Testando listagem de categorias (http)

---
+++ tests/Feature/Http/Controllers/Api/CategoryControllerTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Api;
+
+use App\Models\Category;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class CategoryControllerTest extends TestCase
+{
+    use DatabaseMigrations;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testIndex()
+    {
+        $category = factory(Category::class)->create();
+        $response = $this->get(route('categories.index'));
+
+        $response->assertStatus(200)->assertJson([$category->toArray()]);
+    }
+
+    public function testShow()
+    {
+        $category = factory(Category::class)->create();
+        $response = $this->get(route('categories.show', ['category' => $category->id]));
+
+        $response->assertStatus(200)->assertJson($category->toArray());
+    }
+}
