6745462c4a0b1fbedafc4eca27e9349927f20cc2
Dhoi Almeida <26941289+dhoialmeida@users.noreply.github.com>
2020-11-30 12:12:58 -0300

    AssertUpdate

--- tests/Feature/Http/Controllers/Api/CategoryControllerTest.php
+++ tests/Feature/Http/Controllers/Api/CategoryControllerTest.php
@@ -78,33 +78,35 @@ class CategoryControllerTest extends TestCase
 
     public function testUpdate()
     {
-        $response = $this->json('PUT', route('categories.update', ['category' => $this->category->id]), [
+        $this->category = factory(Category::class)->create([
+            'description' => 'description',
+            'is_active' => false,
+        ]);
+
+        // Atualização genérica
+        $data = [
             'name' => 'test',
             'description' => 'test',
             'is_active' => true,
+        ];
+        $response = $this->assertUpdate($data, $data + ['deleted_at' => null]);
+        $response->assertJsonStructure([
+            'created_at', 'updated_at'
         ]);
 
-        $id = $response->json('id');
-        $category = Category::find($id);
-
-        $response
-            ->assertStatus(200)
-            ->assertJson($category->toArray())
-            ->assertJsonFragment([
-                'description' => 'test',
-                'is_active' => true
-            ]);
-
-        // ###############
-        $response = $this->json('PUT', route('categories.update', ['category' => $category->id]), [
+        // Description vazio
+        $data = [
             'name' => 'test',
             'description' => '',
-        ]);
+        ];
+        $response = $this->assertUpdate($data, array_merge($data, ['description' => null]));
 
-        $response
-            ->assertJsonFragment([
-                'description' => null,
-            ]);
+        // Description alterado para não ser mais vazio
+        $data = [
+            'name' => 'test',
+            'description' => 'test',
+        ];
+        $response = $this->assertUpdate($data, array_merge($data, ['description' => 'test']));
     }
 
     public function testDestroy()
--- tests/Traits/TestSaves.php
+++ tests/Traits/TestSaves.php
@@ -10,10 +10,30 @@ trait TestSaves {
         if ($response->status() !== 201) {
             throw new \Exception("Status must be 201, given {$response->status()}: {$response->content()}.");
         } 
+        
+        $this->assertInDatabase($response, $testDatabase);
+        $this->arrestJsonResponseContent($response, $testDatabase, $testJsonData);
+        return $response;
+    }
+
+    protected function assertUpdate($sendData, $testDatabase, $testJsonData = []): TestResponse {
+        $response = $this->json('PUT', $this->routeUpdate(), $sendData);
+        if ($response->status() !== 200) {
+            throw new \Exception("Status must be 200, given {$response->status()}: {$response->content()}.");
+        } 
+        
+        $this->assertInDatabase($response, $testDatabase);
+        $this->arrestJsonResponseContent($response, $testDatabase, $testJsonData);
+        return $response;
+    }
+
+    private function assertInDatabase($response, $testDatabase) {
         $model = $this->model();
         $table = (new $model)->getTable();
         $this->assertDatabaseHas($table, $testDatabase + ['id' => $response->json("id")]);
+    }
 
+    private function arrestJsonResponseContent($response, $testDatabase, $testJsonData) {
         $testResponse = $testJsonData ?? $testDatabase;
         $response->assertJsonFragment($testJsonData + ['id' => $response->json('id')]);
         return $response;
