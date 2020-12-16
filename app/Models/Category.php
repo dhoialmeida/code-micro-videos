02c73a30cf22bcf70d347b0e188d608945b73874
Dhoi Almeida <26941289+dhoialmeida@users.noreply.github.com>
2020-12-16 15:46:09 -0300

    Relacionando vídeos com Category e Genre

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Category extends Model
 {
     use SoftDeletes, Traits\Uuid;
+
     protected $fillable = ['name', 'description', 'is_active'];
     protected $dates = ['deleted_at'];
     protected $casts = [
